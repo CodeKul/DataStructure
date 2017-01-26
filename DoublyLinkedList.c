f750806adbb391806b78c350449f05eacd4b6b0d
Varun <varun@Varuns-MacBook-Air.local>
2017-01-26 13:48:58 +0530

    added new functions to DiiublyLinkedList.c

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -43,6 +43,131 @@ void addAtEnd(struct List **q, int d) {
     }
 }
 
+int getCount(struct List *q)
+{
+	int i=0;
+	while(q != NULL)
+	{
+		q=q->next;
+		i++;
+	}
+	return i;
+	
+}
+
+void addAtBeg(struct List **q,int num)
+{
+    struct List *temp;
+	if(*q == NULL)
+	{
+		temp=malloc(sizeof(struct List));
+		temp->data=num;
+		temp->next=NULL;
+		temp->prev=NULL;
+		(*q)=temp;
+		
+	}
+	else
+	{
+		temp=malloc(sizeof(struct List));
+		temp->data=num;
+		temp->next=*q;
+		temp->prev=NULL;
+		(*q)->prev=temp;
+		(*q)=temp;		
+	}
+}
+
+void addAtIndex(struct List **q,int loc,int num)
+{
+
+	int count,i=1;
+	struct List *temp,*s; 
+
+	count=getCount(*q);
+
+	if(loc>count)
+	{
+		printf("inside getcount");
+		printf("Their are not enough nodes in the linked list\n");
+		return;
+
+	}
+	if(loc==0)
+	{
+		addAtBeg(q,num);
+        	return;     
+	}
+	if(loc==count)
+	{
+		addAtEnd(q,num);
+		return;
+	}
+	s=*q;
+	while(i<loc)
+	{
+		s = s->next;
+		i++;
+	}
+
+	temp=malloc(sizeof(struct List));
+	temp->data=num;
+	temp->next=s;
+	temp->prev=s->prev;
+	(s->prev)->next=temp;
+	s->prev=temp;
+}
+
+void delAtIndex(struct List **q,int loc)
+{
+	int count,i=1;
+        struct List *temp,*s; 
+
+	count=getCount(*q);
+
+	if(loc>count)
+	{
+		printf("inside getcount");
+		printf("Their are not enough nodes in the linked list\n");
+		return;
+	}
+	if(loc==1)
+	{
+		*q==NULL;
+		return;
+	}
+	if(loc==count)
+	{
+		temp=*q;
+		while(temp->next !=NULL)
+		{
+			temp=temp->next;
+		}
+		(temp->prev)->next=NULL;
+		return;
+        }
+	s=*q;
+	while(i<loc)
+	{
+		s=s->next;
+		i++;
+  	}
+	(s->prev)->next=s->next;
+	(s->next)->prev=s->prev;
+	free(s);
+}
+
+int getSumOfData(struct List *q)
+{
+	int sum=0;
+	while(q != NULL)	
+	{
+		sum=sum+(q->data);
+		q=q->next;
+	}
+	return sum;
+}
+
 void display(struct List *q)
 {
     while(q != NULL)
@@ -55,11 +180,28 @@ void display(struct List *q)
 }
 int main() {
     
-    struct List *start = NULL;
+    	struct List *start = NULL;
+    	int y,z;
     
-    addAtEnd(&start,12);
-     addAtEnd(&start,34);
+	addAtEnd(&start,12);
+	addAtEnd(&start,34);
+	addAtEnd(&start,39);
+	addAtEnd(&start,79);
+	addAtEnd(&start,19);
     
-    display(start);
-    
+	display(start);
+	//y=getCount(start);
+	// printf("the total nodes are %d",y);
+  	addAtBeg(&start,78);    
+   	display(start);
+
+    	addAtIndex(&start,3,67);
+   	display(start);
+
+	delAtIndex(&start,2);
+	display(start);
+	
+	z=getSumOfData(start);
+	printf("sum=%d\n",z);
+
 }

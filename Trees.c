31721ee67f889dcdbe4badf50ce6d2c95f803a79
Varun <varun@Varuns-MacBook-Air.local>
2017-02-19 17:31:08 +0530

    search, delete

--- Trees.c
+++ Trees.c
@@ -9,6 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
 struct treeNode
 {
     struct treeNode *left;
@@ -37,6 +40,158 @@ void insert(struct treeNode **q,int d)
         }
     }
 }
+
+void search(struct treeNode **q1,struct treeNode **x1,struct treeNode **parent,int num,int *found)
+{
+    struct treeNode *q2;
+    q2=*q1;
+    *found=FALSE;
+    *parent=NULL;
+    
+    while(q2 != NULL)
+    {
+        if(q2->data == num)
+        {
+            *found=TRUE;
+            *x1=q2;
+            return;
+        }
+        *parent=q2;
+        
+        if(num>(q2->data))
+        {
+            q2=q2->right;
+        }
+        else
+        {
+            q2=q2->left;
+        }
+    }
+}
+
+void delete(struct treeNode **q,int num)
+{
+    int found;
+    struct treeNode *x,*parent,*xsucc;
+    
+    if(*q == NULL)
+    {
+        printf("the tree is empty\n");
+    }
+    
+    parent=x=NULL;
+    
+    search(q,&x,&parent,num,&found);
+    
+    if(found == FALSE)
+    {
+        printf("The data node not found\n");
+        return;
+    }
+    
+    if (parent == NULL) {
+        if(x->left != NULL && x->right != NULL)
+        {
+            parent=x;
+            xsucc=x->right;
+            x=x->left;
+
+            while(x->right != NULL)
+            {
+                x=x->right;
+            }
+            x->right = xsucc;
+            *q = parent->left;
+            free(parent);
+            return;
+        }
+        
+        if(x->left == NULL && x->right == NULL)
+        {
+            *q=NULL;
+            free(x);
+            printf("there is no node in the tree");
+            return;
+        }
+        
+        if(x->left == NULL && x->right != NULL)
+        {
+            printf("right not NULL\nleft not NULL\n");
+            parent=x;
+            x=x->right;
+            *q=x;
+            free(parent);
+            return;
+        }
+        if(x->left != NULL && x->right == NULL)
+        {
+            parent=x;
+            x=x->left;
+            *q=x;
+            free(parent);
+            return;
+        }
+    }
+    
+    if(x->left != NULL && x->right != NULL)
+    {
+        parent=x;
+        xsucc=x->right;
+        
+        while(xsucc->left != NULL)
+        {
+            parent=xsucc;
+            xsucc=xsucc->left;
+        }
+        
+        x->data=xsucc->data;
+        x=xsucc;
+    }
+    
+
+    if(x->left != NULL && x->right == NULL)
+    {
+        if(parent->left == x)
+        {
+            parent->left = x->left;
+        }
+        if(parent->right == x)
+        {
+            parent->right = x->right;
+        }
+        free(x);
+        return;
+    }
+    
+    if(x->left == NULL && x->right == NULL)
+    {
+        if(parent->left == x)
+        {
+            parent->left=NULL;
+        }
+        if(parent->right == x)
+        {
+            parent->right=NULL;
+        }
+        free(x);
+        return;
+    }
+    
+    if(x->left == NULL && x->right != NULL)
+    {
+        if(parent->left == x)
+        {
+            parent->left=x->right;
+        }
+        if(parent->right == x)
+        {
+            parent->right=x->right;
+        }
+        free(x);
+        return;
+    }
+}
+
 
 void inorder(struct treeNode *tn)
 {
@@ -78,9 +233,9 @@ int main()
     
     tn=NULL;
     
-    int arr[]={40,20,10,30,60,80,90};
+    int arr[]={40,20,10,30};
     
-    for(i=0;i<7;i++)
+    for(i=0;i<4;i++)
     {
         insert(&tn,arr[i]);
     }
@@ -89,12 +244,30 @@ int main()
     inorder(tn);
     printf("\n");
 
-    printf("preorder\n");
-    preorder(tn);
-    printf("\n");
+    //printf("preorder\n");
+    //preorder(tn);
+    //printf("\n");
     
-    printf("postorder\n");
-    postorder(tn);
+    //printf("postorder\n");
+    //postorder(tn);
+    //printf("\n");
+    
+//    delete(&tn,10);
+//    delete(&tn,30);
+//    delete(&tn,60);
+//    inorder(tn);
+
+//    delete(&tn,80);
+//    delete(&tn,90);
+//    delete(&tn,20);
+//    inorder(tn);
+    
+    
+    delete(&tn,40);
+
+    inorder(tn);
+    printf("\n");
+    preorder(tn);
     printf("\n");
 
 }
